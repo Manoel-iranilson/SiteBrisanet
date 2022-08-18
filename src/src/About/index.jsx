45064818ab7d8b309b4d9c848bf6c25d6f8c77a1
Manoel Iranilson <manoelvieirafilho0@gmail.com>
2022-08-18 07:35:04 -0300

    Criação de animação

--- src/src/About/index.jsx
+++ src/src/About/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import { MdEast } from "react-icons/md";
+import {useLottie} from "lottie-react"
+import internet from '../../assets/internet.json'
 
 function About() {
+
+  const options = {
+    animationData: internet,
+    loop: true
+  };
+  const { View } = useLottie(options);
+    
   return ( 
   <section>
     <div className="row text-center text-md-start">
@@ -10,6 +19,9 @@ function About() {
         <h1  className="display-2 fw-bold"> Brisanet</h1>
           <p className="text-muted">Com a Brisanet você tem muito mais vantagens! Como assinante, você ganha acesso a diversos serviços para aproveitar mais ainda o seu plano.</p>
           <a className="btn w-50 p-3 fw-bold text-white " style={{backgroundColor:"#F56E28"}}>Assine Já<MdEast className='mx-1' size={20}/></a>
+        </div>
+        <div className="col-md-6">
+          {View}
         </div>
     </div>
   </section>
